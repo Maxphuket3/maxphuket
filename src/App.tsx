e97e7b494cacc2b07ebc1d04e5fc8f24ce9eed1e
MaxPhuket Dev <dev@maxphuket.com>
2026-02-14 10:11:32 +0700

    Complete overhaul of App.tsx structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { JourneyProvider } from './context/JourneyContext';
-import MainScreen from './screens/MainScreen';
+import JourneyStartScreen from './screens/JourneyStartScreen';
+import JourneySelectScreen from './screens/JourneySelectScreen';
+import LastDayScreen from './screens/LastDayScreen';
 import TourDetailScreen from './screens/TourDetailScreen';
-import ReservationScreen from './screens/ReservationScreen';
-import ConfirmationScreen from './screens/ConfirmationScreen';
+import MainScreen from './screens/MainScreen';
 import AdminDashboard from './screens/AdminDashboard';
 import ProductModal from './components/ProductModal';
 
@@ -14,14 +15,18 @@ const App: React.FC = () => {
       <BrowserRouter>
         <div className="App">
           <Routes>
-            <Route path="/" element={<MainScreen />} />
-            <Route path="/tour/:id" element={<TourDetailScreen />} />
-            <Route path="/reservation" element={<ReservationScreen />} />
-            <Route path="/confirmation" element={<ConfirmationScreen />} />
+            {/* 메인 여정 흐름 */}
+            <Route path="/" element={<JourneyStartScreen />} />
+            <Route path="/select" element={<JourneySelectScreen />} />
+            <Route path="/result" element={<LastDayScreen />} />
+            <Route path="/tour-detail/:id" element={<TourDetailScreen />} />
+
+            {/* 기타 화면 */}
+            <Route path="/old-main" element={<MainScreen />} />
             <Route path="/admin" element={<AdminDashboard />} />
           </Routes>
 
-          {/* 상품 상세 모달 연결 - 이 줄이 있어야 클릭 시 팝업이 뜹니다 */}
+          {/* 상품 상세 모달 - 여기서 관리되어야 클릭 시 정상 작동합니다 */}
           <ProductModal product={null} isOpen={false} onClose={() => { }} />
         </div>
       </BrowserRouter>
